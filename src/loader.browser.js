dab127688474bb4c5a3e5f17f99e5e6f45d24e6e
Samm Zuest Cooper <samm@hfour.ca>
2019-09-03 13:29:40 -0700

    port loader api to browser

--- src/loader.browser.js
+++ src/loader.browser.js
@@ -2,15 +2,29 @@ const EMBDLoaderBase = require('./loader.base')
 
 const _ = require('lodash')
 const URL = require('url')
-const posixpath = require('path')
+const pathlib = require('path')
 const axios = require('axios')
 
 class EMBDLoaderBrowser extends EMBDLoaderBase {
   extname( url ) {
     let { pathname } = URL.parse( url )
-    let extname = posixpath.extname( pathname )
+    let extname = pathlib.extname( pathname )
     extname = _.trimStart( extname, '.' ).toLowerCase()
     return extname
+  }
+
+  dirname( pathname ) {
+    let dirname = pathlib.dirname( pathname )
+    return dirname
+  }
+
+  resolve() {
+    let args = _.filter( arguments )
+    // args = args.map()
+    args.unshift('/')
+    let file = pathlib.resolve.apply( null, args )
+    file = file.substr(1)
+    return file
   }
 
   async load( url ) {
